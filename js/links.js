ee9797ce0a7c8f50ccda4584059beba8b2ba0af0
datamadic <xavier@openfin.co>
2014-09-15 09:27:21 -0400

    updated some of the copy, added links to annotated source

--- js/links.js
+++ js/links.js
@@ -1,27 +1,14 @@
 /*global fin*/
 
-// open links the a browser 
+// open links in a browser 
 (function initLinks() {
     'use strict';
 
-    [].forEach.call(document.querySelectorAll('.openfin-gallery'), function(item) {
+    [].forEach.call(document.querySelectorAll('.browser-link'), function(item) {
         item.addEventListener('click', function() {
             fin.desktop
                 .System
-                .openUrlWithBrowser('http://openfin.co/app-gallery.html');
-        });
-    });
-})();
-
-// open the annotated source 
-(function initLinks() {
-    'use strict';
-
-    [].forEach.call(document.querySelectorAll('.openfin-gallery'), function(item) {
-        item.addEventListener('click', function() {
-            fin.desktop
-                .System
-                .openUrlWithBrowser('http://openfin.co/app-gallery.html');
+                .openUrlWithBrowser(item.getAttribute('destination'));
         });
     });
 })();
